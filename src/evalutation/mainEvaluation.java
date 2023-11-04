596e7823d4bdce775d47afbb18b4b7db8c03e7e7
inesimoes01 <m.inesimoes01@gmail.com>
2023-11-04 15:26:14 +0000

    initial structure with the requirmentes

---
+++ src/evalutation/mainEvaluation.java
@@ -0,0 +1,11 @@
+public class mainEvaluation {
+    public static void main(String[] args) {
+
+        //evaluation of the systems using a standard collection: 
+            //TREC DL 2019 queries and TREC DL 2019 qrels
+            // OR
+            //TREC DL 2020 queries and TREC DL 2020 qrels
+
+        
+    }
+}

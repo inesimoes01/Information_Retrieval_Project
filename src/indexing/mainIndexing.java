27221386d31af5b3e9f31261c660a7d7b11d998b
inesimoes01 <m.inesimoes01@gmail.com>
2023-11-21 12:10:28 +0000

    Revert "Revert "initial structure with the requirmentes""
    This reverts commit 451e8ebeff8073d32fcfe73e6c26cf8dbc4956d9.

---
+++ src/indexing/mainIndexing.java
@@ -0,0 +1,26 @@
+public class mainIndexing {
+
+    // structures: 
+        // lexicon (to store the vocabulary terms and their information) 
+        // document table (to store docid to docno mapping and document lengths)
+
+    // compile flags:
+        // ASCII format or binary format during debugging and for performance, respectively
+        // enalbe/disble stemming & stopword removal
+
+    public static void main(String[] args) {
+
+        // uncompress documents 
+        // (load one compressed file into memory and then call the right library function to uncompress it into another memory-based buffer)
+
+        // for each document
+            // split pid from text
+            // call text processing 
+            // create index
+                // search new terms and add them
+                // add a document to the posting list of a term
+        
+        // index compression (do not use standard compressors such as gzip for index compression)
+                
+    }
+}

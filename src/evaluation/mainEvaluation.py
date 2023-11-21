ec5fbcdb3dbdf4b3bc757db061f574252aa7f6cc
inesimoes01 <m.inesimoes01@gmail.com>
2023-11-21 12:13:45 +0000

    fixing issues from the revert

--- src/evaluation/mainEvaluation.py
+++
@@ -1,8 +0,0 @@
-class mainEvaluation:
-
-    # evaluation of the systems using a standard collection: 
-        # REC DL 2019 queries and TREC DL 2019 qrels
-        # OR
-        # TREC DL 2020 queries and TREC DL 2020 qrels
-
-    pass

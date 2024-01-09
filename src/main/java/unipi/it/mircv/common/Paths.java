dd4b077d7646412029b5dfd6ab706598b0747fb8
inesimoes01 <m.inesimoes01@gmail.com>
2024-01-09 18:49:30 +0000

    path corrected

--- src/main/java/unipi/it/mircv/common/Paths.java
+++ src/main/java/unipi/it/mircv/common/Paths.java
@@ -7,7 +7,7 @@ public class Paths {
     public static final Path PATH_LEXICON = java.nio.file.Paths.get("data/output/LexiconMerged.txt");
     public static final Path PATH_INVERTED_INDEX = java.nio.file.Paths.get("data/output/InvertedIndexMerged.txt");
     public static final Path PATH_DOCUMENT_INDEX = java.nio.file.Paths.get("data/output/DocumentIndexMerged.txt");
-    public static final String PATH_EVALUATION_RESULTS = "data/evaluation/input/";
+    public static final String PATH_EVALUATION_RESULTS = "data/evaluation/output/";
     public static final String PATH_EVALUATION_INPUT = "data/evaluation/input/msmarco-test2019-queries.tsv.gz";
     public static final Path PATH_AVGDOCLEN = java.nio.file.Paths.get("data/output/avgDocLen.txt");
 }

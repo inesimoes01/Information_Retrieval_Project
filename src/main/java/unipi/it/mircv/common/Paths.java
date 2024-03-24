2bbadd53c830200d2e40bb75da27fd8a56930cc8
Inês Simões <m.inesimoes01@gmail.com>
2024-03-24 17:49:53 +0000

    paths corrected for querying

--- src/main/java/unipi/it/mircv/common/Paths.java
+++ src/main/java/unipi/it/mircv/common/Paths.java
@@ -4,9 +4,9 @@ import java.nio.file.Path;
 
 public class Paths {
     public static final String PATH_COLLECTION = "data/input/collection.tar.gz";
-    public static final Path PATH_LEXICON = java.nio.file.Paths.get("data/output/LexiconMerged.txt");
-    public static final Path PATH_INVERTED_INDEX = java.nio.file.Paths.get("data/output/InvertedIndexMerged.txt");
-    public static final Path PATH_DOCUMENT_INDEX = java.nio.file.Paths.get("data/output/DocumentIndexMerged.txt");
+    public static final Path PATH_LEXICON = java.nio.file.Paths.get("data/output/merged/LexiconMerged.txt");
+    public static final Path PATH_INVERTED_INDEX = java.nio.file.Paths.get("data/output/merged/InvertedIndexMerged.txt");
+    public static final Path PATH_DOCUMENT_INDEX = java.nio.file.Paths.get("data/output/merged/DocumentIndexMerged.txt");
     public static final String PATH_EVALUATION_RESULTS = "data/evaluation/output/";
     public static final String PATH_EVALUATION_INPUT = "data/evaluation/input/msmarco-test2019-queries.tsv.gz";
     public static final Path PATH_AVGDOCLEN = java.nio.file.Paths.get("data/output/avgDocLen.txt");
